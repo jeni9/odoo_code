979f77b652d10d5c5a22c76f9460e568dcb83eca
“jensee” <jenseechovatiya09@gmail.com>
2021-11-28 20:05:37 +0530

    add new file

---
+++ book/__manifest__.py
@@ -0,0 +1,14 @@
+{
+  'name':'my library',
+  'category':'Sales',
+  'application':True,
+
+  'data':[
+
+      'views/book_menu.xml',
+      'security/ir.model.access.csv',
+      'views/book_view.xml',
+  ]
+
+
+}
